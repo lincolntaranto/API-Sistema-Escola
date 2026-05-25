c18fe59c6946ea758c3401c322b592cc2ec3f85f
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-25 00:55:04 -0300

    refactor: muda a funcao criar_conta para que ela utilize a classe schema usuario.

--- API/sistema-escola/routes/auth_routes.py
+++ API/sistema-escola/routes/auth_routes.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from security import hash_senha
+
 from models import Usuario
 from models.session import get_session
+
+from schemas.usuario import UsuarioSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -13,13 +16,18 @@ async def autenticar():
     return {"mensagem" : "Rota de autenticação"}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(nome: str, senha: str, cargo: int, email: str, numero: str, session: Session = Depends(get_session)):
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(get_session)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         raise HTTPException(status_code=400, detail="email já cadastrado!")
     else:
-        senha_criptografada = hash_senha(senha)
-        novo_usuario = Usuario(nome = nome, senha = senha_criptografada, cargo = cargo, email = email, numero = numero)
+        senha_criptografada = hash_senha(usuario_schema.senha)
+        novo_usuario = Usuario(
+            nome = usuario_schema.nome,
+            senha = senha_criptografada,
+            cargo = usuario_schema.cargo,
+            email = usuario_schema.email,
+            numero = usuario_schema.numero)
         session.add(novo_usuario)
         session.commit()
         session.refresh(novo_usuario)
