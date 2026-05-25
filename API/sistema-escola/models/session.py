b1402df68957b4cebd1cd51f8b3e0e7f4d8733cb
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-24 23:29:23 -0300

    feat: adiciona o model session

---
+++ API/sistema-escola/models/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import sessionmaker
+from .base import db
+
+SessionLocal = sessionmaker(bind=db)
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
