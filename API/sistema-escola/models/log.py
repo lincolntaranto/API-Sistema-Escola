b249f44b6d0ce052e7da0e2d5b2dbc748c51f4b0
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-29 08:35:33 -0300

    feat: adiciona o model de Log

---
+++ API/sistema-escola/models/log.py
@@ -0,0 +1,14 @@
+import datetime
+
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+
+from .base import Base
+
+class Log(Base):
+    __tablename__ = "logs"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id_usuario = Column(ForeignKey("usuarios.id"), nullable=False)
+    acao = Column(String(100), nullable=False)
+    descricao = Column(String(100), nullable=True)
+    data_hora = Column(DateTime, default=lambda: datetime.now(datetime.timezone.utc), nullable=False)
