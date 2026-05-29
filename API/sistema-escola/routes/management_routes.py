fcae96f15ef38aa1aa376e2235574cdb246fbe55
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-29 08:55:44 -0300

    feat: adiciona logs nas rotas cadastrar_aluno e apagar_aluno

--- API/sistema-escola/routes/management_routes.py
+++ API/sistema-escola/routes/management_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from models.log import Log
 from models.session import get_session
-from models import Aluno
+from models import Aluno, Usuario, usuario
 from schemas.aluno import AlunoSchema
 from security import verificar_token
 
@@ -14,7 +15,7 @@ async def mostrar_alunos():
     return {"mensagem" : "Você acessou a rota de alunos"}
 
 @management_router.post("/cadastrar_aluno")
-async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(get_session)):
+async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(get_session), usuario: Usuario = Depends(verificar_token)):
     """Rota para cadastrar um aluno no sistema"""
     aluno= session.query(Aluno).filter(Aluno.nome == aluno_schema.nome,
                                        Aluno.data_nascimento == aluno_schema.data_nascimento,
@@ -28,6 +29,12 @@ async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(
         nome_responsavel=aluno_schema.nome_responsavel,
         celular_responsavel=aluno_schema.numero_responsavel
     )
+    log = Log(
+        id_usuario=usuario.id,
+        acao= "cadastrar_aluno",
+        descricao=f"Aluno {novo_aluno.nome}, da turma {novo_aluno.turma} foi cadastrado."
+    )
+    session.add(log)
     session.add(novo_aluno)
     session.commit()
     session.refresh(novo_aluno)
@@ -39,11 +46,17 @@ async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(
     }
 
 @management_router.delete("/apagar_aluno")
-async def apagar_aluno(id_aluno: int, session: Session = Depends(get_session)):
+async def apagar_aluno(id_aluno: int, session: Session = Depends(get_session), usuario: Usuario = Depends(verificar_token)):
     aluno = session.query(Aluno).filter(Aluno.id == id_aluno).first()
     if not aluno:
         raise HTTPException(status_code=404, detail="ID de aluno inexistente!")
     session.delete(aluno)
+    log = Log(
+        id_usuario=usuario.id,
+        acao="deletar_aluno",
+        descricao=f"Aluno {aluno.nome}, da turma {aluno.turma} foi deletado."
+    )
+    session.add(log)
     session.commit()
     return {
         "mensagem": "Aluno deletado com sucesso!",
