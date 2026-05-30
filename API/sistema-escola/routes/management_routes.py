62ed2680199a6776ff7d3947b2cc7852aab67536
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-30 14:36:26 -0300

    feat: adiciona a logica da rota de consultar alunos (/alunos)

--- API/sistema-escola/routes/management_routes.py
+++ API/sistema-escola/routes/management_routes.py
@@ -7,16 +7,37 @@ from models import Aluno, Usuario, usuario
 from schemas.aluno import AlunoSchema
 from security import verificar_token
 
-management_router = APIRouter(prefix="/management", tags=["management"], dependencies=[Depends(verificar_token)])
+management_router = APIRouter(prefix="/management", tags=["management"])
 
 @management_router.get("/alunos")
-async def mostrar_alunos():
-    """"Rota para mostrar a lista de alunos no sistema."""
-    return {"mensagem" : "Você acessou a rota de alunos"}
+async def mostrar_alunos(id_aluno: int,
+                         session: Session = Depends(get_session),
+                         usuario: Usuario = Depends(verificar_token)):
+    """"Rota para consultar alunos registrados no sistema."""
+
+    aluno = session.query(Aluno).filter(Aluno.id == id_aluno).first()
+    if not aluno:
+        raise HTTPException(status_code=404, detail="ID de aluno inexistente!")
+    log = Log(
+        id_usuario=usuario.id,
+        acao="consultar_aluno",
+        descricao=f"Aluno {aluno.nome}, da turma {aluno.turma} foi consultado."
+    )
+    session.add(log)
+    session.commit()
+    return {"nome" : aluno.nome,
+            "turma": aluno.turma,
+            "data_nascimento" : aluno.data_nascimento,
+            "responsavel" : aluno.nome_responsavel,
+            "celular_responsavel" : aluno.celular_responsavel}
 
 @management_router.post("/cadastrar_aluno")
-async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+async def cadastrar_aluno(aluno_schema: AlunoSchema,
+                          session: Session = Depends(get_session),
+                          usuario: Usuario = Depends(verificar_token)):
+
     """Rota para cadastrar um aluno no sistema"""
+
     aluno= session.query(Aluno).filter(Aluno.nome == aluno_schema.nome,
                                        Aluno.data_nascimento == aluno_schema.data_nascimento,
                                        Aluno.nome_responsavel == aluno_schema.nome_responsavel).first()
@@ -46,7 +67,12 @@ async def cadastrar_aluno(aluno_schema: AlunoSchema, session: Session = Depends(
     }
 
 @management_router.delete("/apagar_aluno")
-async def apagar_aluno(id_aluno: int, session: Session = Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+async def apagar_aluno(id_aluno: int,
+                       session: Session = Depends(get_session),
+                       usuario: Usuario = Depends(verificar_token)):
+
+    """Rota para deletar um aluno do sistema"""
+
     aluno = session.query(Aluno).filter(Aluno.id == id_aluno).first()
     if not aluno:
         raise HTTPException(status_code=404, detail="ID de aluno inexistente!")
