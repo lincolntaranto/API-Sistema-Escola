5e7f7e0a1fa8b2c735bdc05911302633ecd02dec
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-31 07:42:34 -0300

    feat: adiciona a coluna id_aluno

--- API/sistema-escola/models/log.py
+++ API/sistema-escola/models/log.py
@@ -9,6 +9,7 @@ class Log(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     id_usuario = Column(ForeignKey("usuarios.id"), nullable=False)
+    id_aluno = Column(ForeignKey("alunos.id"))
     acao = Column(String(100), nullable=False)
     descricao = Column(String(100), nullable=True)
     data_hora = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
