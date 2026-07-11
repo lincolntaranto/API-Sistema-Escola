06b44bedc23949ee7d216e871f2257bc5c778b01
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-11 18:49:32 -0300

    test: cria o teste 'test_consult_noexistent_student'

--- API/sistema-escola/tests/test_management.py
+++ API/sistema-escola/tests/test_management.py
@@ -5,3 +5,12 @@ def test_consult_student(client, token):
         headers={"Authorization": f"Bearer {token}"},
     )
     assert "nome" in response.json()
+
+
+def test_consult_nonexistent_stundet(client, token):
+    response = client.get(
+        "/management/alunos",
+        params={"id_aluno": 200},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 404
