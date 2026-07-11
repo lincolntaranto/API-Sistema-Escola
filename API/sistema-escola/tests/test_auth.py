e7d84543aa4c7de48bf546fe80a8ad01e467cec9
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-11 15:47:39 -0300

    test: cria o test 'test_create_user_wrong_email'

--- API/sistema-escola/tests/test_auth.py
+++ API/sistema-escola/tests/test_auth.py
@@ -11,3 +11,18 @@ def test_create_user(client, invite):
         },
     )
     assert response.status_code == 200
+
+
+def test_create_user_wrong_email(client, invite):
+    response = client.post(
+        "/auth/criar_conta",
+        json={
+            "nome": "Teste",
+            "email": "pytesteemail.com",
+            "senha": "123",
+            "cargo": 1,
+            "numero": "123",
+            "convite": invite,
+        },
+    )
+    assert response.status_code == 422
