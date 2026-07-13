aaf42f7ae2234cf60f6fd9f000e7f6f143cbeb54
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-13 12:39:38 -0300

    test: cria o teste 'test_delete_non_existent_classroom'

--- API/sistema-escola/tests/test_management.py
+++ API/sistema-escola/tests/test_management.py
@@ -149,3 +149,13 @@ def test_delete_classroom(client, token):
         headers={"Authorization": f"Bearer {token}"},
     )
     assert response.status_code == 200
+
+
+def test_delete_non_existent_classroom(client, token):
+    create_default_classroom(client, token)
+    response = client.delete(
+        "/management/apagar_turma",
+        params={"id_turma": 404},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 404
