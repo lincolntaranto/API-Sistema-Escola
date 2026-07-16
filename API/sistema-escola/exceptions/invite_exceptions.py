5564867372fc5f990ddca3b5fa09c196bb97cc1f
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-16 02:32:42 -0300

    feat: cria a exception 'UsedInvitation'

--- API/sistema-escola/exceptions/invite_exceptions.py
+++ API/sistema-escola/exceptions/invite_exceptions.py
@@ -2,3 +2,9 @@ class InvalidInvite(Exception):
     """Levantada quando um convite não é valido"""
 
     pass
+
+
+class UsedInvitation(Exception):
+    """Levantada quando o convite já foi usado"""
+
+    pass
