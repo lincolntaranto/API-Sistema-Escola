960ee322531bcba8c430f3d7270b5065c2842371
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-16 00:08:46 -0300

    refactor: remove codigo desnecessario

--- API/sistema-escola/models/cargo.py
+++ API/sistema-escola/models/cargo.py
@@ -8,4 +8,4 @@ class Cargo(Base):
     __tablename__ = "cargos"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    nome: Mapped[str] = mapped_column(String(50))
