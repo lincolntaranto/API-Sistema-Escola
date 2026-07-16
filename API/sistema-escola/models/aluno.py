f3169ca33bd649a8bafaef4f8219ef06dc83e1b0
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-16 00:07:56 -0300

    refactor: remove codigo desnecessario

--- API/sistema-escola/models/aluno.py
+++ API/sistema-escola/models/aluno.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Optional
 
-from sqlalchemy import String, ForeignKey, DATE, Boolean
+from sqlalchemy import ForeignKey, DATE
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -11,11 +11,9 @@ class Aluno(Base):
     __tablename__ = "alunos"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    nome: Mapped[str] = mapped_column(String, nullable=False)
-    data_nascimento: Mapped[date] = mapped_column(DATE, nullable=False)
+    nome: Mapped[str] = mapped_column()
+    data_nascimento: Mapped[date] = mapped_column(DATE)
     turma: Mapped[Optional[int]] = mapped_column(ForeignKey("turmas.id"))
-    nome_responsavel: Mapped[str] = mapped_column(String, nullable=False)
-    celular_responsavel: Mapped[str] = mapped_column(String, nullable=False)
-    deletado: Mapped[bool] = mapped_column(
-        Boolean, default=False, nullable=False, server_default="false"
-    )
+    nome_responsavel: Mapped[str] = mapped_column()
+    celular_responsavel: Mapped[str] = mapped_column()
+    deletado: Mapped[bool] = mapped_column(default=False, server_default="false")
