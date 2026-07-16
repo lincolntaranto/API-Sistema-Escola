730efb7245e81d5e7a290fd2d84ab5610e6ca281
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-15 22:22:26 -0300

    refactor: migra o model 'Convite' para o padrão SQLAlchemy 2.0

--- API/sistema-escola/models/convites.py
+++ API/sistema-escola/models/convites.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Boolean
+from sqlalchemy import Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 
 from models import Base
 
@@ -6,5 +7,5 @@ from models import Base
 class Convite(Base):
     __tablename__ = "convites"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    usado = Column(Boolean, default=False, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    usado: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
