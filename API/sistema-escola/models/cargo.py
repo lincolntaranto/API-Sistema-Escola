a801c4e3f3b6afab84bb82bfe7639c9d78e0d02d
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-15 22:18:23 -0300

    refactor: migra o model 'Cargo' para o padrão SQLAlchemy 2.0

--- API/sistema-escola/models/cargo.py
+++ API/sistema-escola/models/cargo.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
@@ -6,5 +7,5 @@ from .base import Base
 class Cargo(Base):
     __tablename__ = "cargos"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    nome = Column(String(50), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
