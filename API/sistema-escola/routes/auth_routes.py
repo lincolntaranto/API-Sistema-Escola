60ffa3f67ae8b5493beae4617d444454a417b6eb
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-18 17:54:46 -0300

    refactor: migra a rota de login para arquitetura em camadas

--- API/sistema-escola/routes/auth_routes.py
+++ API/sistema-escola/routes/auth_routes.py
@@ -1,19 +1,21 @@
-from datetime import timedelta
-
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy.orm import Session
 
 from schemas.login import LoginSchema
-from services.auth import verify_refresh_token, autenticar_usuario
+from services.auth import (
+    verify_refresh_token,
+    autenticar_usuario,
+    create_user,
+    login_user,
+)
 from core.security import criar_token
 
 from models import Usuario
 from models.session import get_session
 
 from schemas.usuario import UsuarioSchema
-from services.user import create_user
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -39,19 +41,16 @@ async def criar_conta(
 
 @auth_router.post("/sessions")
 async def login(login_schema: LoginSchema, session: Session = Depends(get_session)):
-    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
-    if not usuario:
-        raise HTTPException(
-            status_code=400, detail="usuário não encontrado ou senha incorreta."
-        )
-    else:
-        access_token = criar_token(usuario.id, "access")
-        refresh_token = criar_token(usuario.id, "refresh", timedelta(days=5))
-        return {
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "token_type": "Bearer",
-        }
+    login_u = login_user(login_schema=login_schema, session=session)
+
+    access_token = login_u["access_token"]
+    refresh_token = [login_u["refresh_token"]]
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "Bearer",
+    }
 
 
 @auth_router.post("/login-form")
