8efa0043f247ea220e9eaa193eab8682471f1ed5
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-17 21:56:07 -0300

    feat: cria a funcao 'create_user'

--- API/sistema-escola/services/user.py
+++ API/sistema-escola/services/user.py
@@ -1,8 +1,11 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from exceptions.user_exceptions import UserNotFound
+from core.security import get_password_hash
+from services.convite import verificar_convite
+from exceptions.user_exceptions import UserNotFound, EmailAlreadyRegistered
 from models import Usuario
+from schemas.usuario import UsuarioSchema
 
 
 def get_user_by_id(id_user: int, session: Session) -> Usuario:
@@ -30,3 +33,25 @@ def get_user_by_email_or_none(email: int, session: Session) -> Usuario | None:
     if not user:
         return None
     return user
+
+
+def create_user(usuario_schema: UsuarioSchema, session: Session) -> Usuario:
+    usuario = session.execute(
+        select(Usuario).where(Usuario.email == usuario_schema.email)
+    ).scalar_one_or_none()
+    if usuario:
+        raise EmailAlreadyRegistered
+    else:
+        cargo = int(verificar_convite(usuario_schema.convite, session))
+        senha_criptografada = get_password_hash(usuario_schema.senha)
+        novo_usuario = Usuario(
+            nome=usuario_schema.nome,
+            senha=senha_criptografada,
+            cargo=cargo,
+            email=usuario_schema.email,
+            numero=usuario_schema.numero,
+        )
+        session.add(novo_usuario)
+        session.commit()
+        session.refresh(novo_usuario)
+        return novo_usuario
