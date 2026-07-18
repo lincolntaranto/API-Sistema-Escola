c583f7fe103a344682b22542f28c73a6d89a459d
lincolntaranto <54151306+lincolntaranto@users.noreply.github.com>
2026-07-18 17:53:49 -0300

    feat: cria a funcao 'login_user' e move a 'create_user' para o service auth

--- API/sistema-escola/services/auth.py
+++ API/sistema-escola/services/auth.py
@@ -1,12 +1,23 @@
+from datetime import timedelta
+
 import jwt
 from fastapi import Depends
 from jwt import InvalidTokenError
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from core.config import oauth2_schema, settings
-from core.security import ALGORITHM, verify_password
-from exceptions.user_exceptions import AccessDenied
+from core.security import ALGORITHM, verify_password, get_password_hash, criar_token
+from exceptions.user_exceptions import (
+    AccessDenied,
+    EmailAlreadyRegistered,
+    UserNotFoundOrIncorrectPassword,
+)
+from models import Usuario
 from models.session import get_session
+from schemas.login import LoginSchema
+from schemas.usuario import UsuarioSchema
+from services.convite import verificar_convite
 from services.user import get_user_by_id_or_none, get_user_by_email_or_none
 
 
@@ -51,3 +62,36 @@ def autenticar_usuario(email, senha, session):
     elif not verify_password(senha, usuario.senha):
         return False
     return usuario
+
+
+def create_user(usuario_schema: UsuarioSchema, session: Session) -> Usuario:
+    usuario = session.execute(
+        select(Usuario).where(Usuario.email == usuario_schema.email)
+    ).scalar_one_or_none()
+    if usuario:
+        raise EmailAlreadyRegistered
+    else:
+        cargo = int(verificar_convite(usuario_schema.convite, session))
+        senha_criptografada = get_password_hash(usuario_schema.senha)
+        novo_usuario = Usuario(
+            nome=usuario_schema.nome,
+            senha=senha_criptografada,
+            cargo=cargo,
+            email=usuario_schema.email,
+            numero=usuario_schema.numero,
+        )
+        session.add(novo_usuario)
+        session.commit()
+        session.refresh(novo_usuario)
+        return novo_usuario
+
+
+def login_user(login_schema: LoginSchema, session: Session) -> dict:
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
+    if not usuario:
+        raise UserNotFoundOrIncorrectPassword
+
+    return {
+        "access_token": criar_token(usuario.id, "access"),
+        "refresh_token": criar_token(usuario.id, "refresh", timedelta(days=5)),
+    }
